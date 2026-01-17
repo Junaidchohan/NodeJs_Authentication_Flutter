efdd7e078553241acd69591b70cafdae57d1342e
Junaidchohane <junaidchohanedu@gmail.com>
2026-01-17 15:15:29 +0500

    Connection with NodeJs

---
+++ server/index.js
@@ -0,0 +1,18 @@
+// Import all the dependencies here bcz we need to connect our data base in index.js
+const express = require("express");
+const mongoose = require("mongoose");
+
+// if we have have in env then run on it otherewise run on 3000 port
+const PORT = process.env.PORT || 3000;
+// Initilizing express
+const app = express();
+
+//  express.json middlewhere
+// make sure whatever req commming to our server get pass in json formate
+// after listen to our app start a server
+
+
+//  IP = "0.0.0.0" which mean accessable in  enywhere
+app.listen(PORT , "0.0.0.0", ()=> {
+console.log(`connected at port ${PORT}`);
+});
