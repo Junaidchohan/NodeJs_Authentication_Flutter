3624214d9100fcc66765501ca9a55414924f39cb
Junaidchohane <junaidchohanedu@gmail.com>
2026-01-17 20:48:23 +0500

    Create Sign in Route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,12 +10,14 @@ const authRouter = express.Router();
 authRouter.post("/api/signup", async (req, res) =>{
 try{
 const {name, email, password} =req.body;
+    // Check if user exists
     const existingUser = await User.findOne({email});
     if(existingUser){
         return res.status(400).json({msg:"User with same email already exists!" });
     }
-    const hashedPassword = await bcryptjs.hash(password, 8);
 
+     // Create user
+    const hashedPassword = await bcryptjs.hash(password, 8);
     let user = new User({
         email,
         password: hashedPassword,
@@ -23,16 +25,40 @@ const {name, email, password} =req.body;
     });
     user = await user.save();
     res.json(user);
-    // res.status(201).json({ // return only safe fields
-    //   _id: user._id,
-    //   name: user.name,
-    //   email: user.email,
-    // });
+     // Return without password
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
 }catch(e){
 res.status(500).json({error: e.message});
 }
 });
 
 
+// SignIn route
+
+authRouter.post("/api/signin", async (req, res) => {
+try{
+const {email, password} =req.body;
+    // Check if user exists
+    const user = await User.findOne({email});
+       if(!user){
+        return res.status(400).json({msg:"User with this email does not exists!" });
+
+
+    }
+     const isMatch = await bcryptjs.compare(password, user.password);
+     if(!isMatch){
+        return res.status(400).json({msg:"Incorrect password." });
+       }
+
+          const token = jwt.sign({id: user._id}, "passwordKey");
+          res.json({token, ...user.doc});
+}catch(e){
+res.status(500).json({error: e.message});
+}
+});
 
 module.exports = authRouter;
